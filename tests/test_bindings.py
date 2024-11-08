ad4f6269d58a8d232d38245c2c726e203556feff
David Perl <david.perl@diamond.ac.uk>
2024-11-08 10:11:04 +0000

    Import bindings in marked tests

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -2,22 +2,25 @@ import pytest
 
 
 @pytest.mark.needs_librtc6
-class TestBindings:
+def test_add():
     from rtc6_fastcs import bindings
 
-    def test_add(self):
-        sum = self.bindings.add(4, 5)
-        assert sum == 9
-        sum = self.bindings.add(7, 13)
-        assert sum == 20
+    sum = bindings.add(4, 5)
+    assert sum == 9
+    sum = bindings.add(7, 13)
+    assert sum == 20
 
-    def test_ip_conversion(self):
-        result = self.bindings.ip_str_to_int("123.0.0.1")
-        assert result == 16777339
-        back = self.bindings.ip_int_to_str(result)
-        assert back == "123.0.0.1"
 
-        result = self.bindings.ip_str_to_int("123.123.123.123")
-        assert result == 2071690107
-        back = self.bindings.ip_int_to_str(result)
-        assert back == "123.123.123.123"
+@pytest.mark.needs_librtc6
+def test_ip_conversion():
+    from rtc6_fastcs import bindings
+
+    result = bindings.ip_str_to_int("123.0.0.1")
+    assert result == 16777339
+    back = bindings.ip_int_to_str(result)
+    assert back == "123.0.0.1"
+
+    result = bindings.ip_str_to_int("123.123.123.123")
+    assert result == 2071690107
+    back = bindings.ip_int_to_str(result)
+    assert back == "123.123.123.123"
