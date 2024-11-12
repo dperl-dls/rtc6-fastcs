e49318f04b102665f4ac59f47ca91ef84cbe4810
David Perl <david.perl@diamond.ac.uk>
2024-11-12 13:47:36 +0000

    add list tatus

--- src/rtc6_fastcs/bindings/rtc6_bindings.cpp
+++ src/rtc6_fastcs/bindings/rtc6_bindings.cpp
@@ -23,6 +23,10 @@ class rtc_connection_error : public rtc_error
 {
     using rtc_error::rtc_error;
 };
+class rtc_list_error : public rtc_error
+{
+    using rtc_error::rtc_error;
+};
 
 // RTC error codes
 const uint ERROR_NO_ERROR = 0U;
@@ -70,17 +74,22 @@ void init_dll()
     }
 }
 
-std::string failed_text(std::string task, int card, int errorcode)
+std::string failed_text(std::string task, int card, int errorCode)
 {
-    return str(format("%1% for card %2%  failed with error code: %3%") % task % card % errorcode);
+    return str(format("%1% for card %2%  failed with error code: %3%") % task % card % errorCode);
+}
+std::string failed_text(std::string task, int card, int errorCode, int pageNo)
+{
+    return str(format("%1% for card %2%  failed with error code: %3%. Details about this error can be found on page %4% of the manual.") % task % card % errorCode % pageNo);
 }
 
 int load_program_and_correction_files(uint card, char *programFilePath, char *correctionFilePath)
 { // DAT, ETH/OUT and RBF need to be in the current working directory
     const auto loadProgram = n_load_program_file(card, programFilePath);
+    // TODO: improve error messages for specific errors.
     if (loadProgram != ERROR_NO_ERROR)
     {
-        throw rtc_error(failed_text("n_load_program_file", card, loadProgram));
+        throw rtc_error(failed_text("n_load_program_file", card, loadProgram, 489));
     }
     // Acquire board for further access
     const auto acquire = acquire_rtc(card);
@@ -92,7 +101,7 @@ int load_program_and_correction_files(uint card, char *programFilePath, char *co
     const auto loadCorrection = n_load_correction_file(card, correctionFilePath, 1, 2);
     if (loadCorrection != ERROR_NO_ERROR)
     {
-        throw rtc_error(failed_text("n_load_correction_file", card, loadCorrection));
+        throw rtc_error(failed_text("n_load_correction_file", card, loadCorrection, 474));
     }
     // select_cor_table( 1, 0 ); is done internally. Call select_cor_table, if you want a different setting.
     return n_get_serial_number(card);
@@ -160,6 +169,39 @@ int get_last_eth_error()
     return eth_get_last_error();
 }
 
+void init_list_loading(int listNo)
+{
+    const int result = load_list(listNo, 0);
+    if (result != listNo)
+    {
+        throw rtc_list_error(str(format("Initialising list %1% failed with result %2%! Perhaps this list is busy?") % listNo % result));
+    }
+}
+enum ListStatus
+{
+    LOAD1,
+    LOAD2,
+    READY1,
+    READY2,
+    BUSY1,
+    BUSY2,
+    USED1,
+    USED2,
+};
+py::list get_list_statuses()
+{
+    py::list result;
+    int statuses = read_status();
+    for (int status = 0; status != 8; status++)
+    {
+        if (1)
+        {
+            result.append(static_cast<ListStatus>(status));
+        }
+    }
+    return result;
+}
+
 void close_connection()
 {
     // TODO: check if there is anything to release
@@ -176,6 +218,7 @@ PYBIND11_MODULE(rtc6_bindings, m)
     m.doc() = "bindings for the scanlab rtc6 ethernet laser controller"; // optional module docstring
     py::register_exception<rtc_error>(m, "RtcError");
     py::register_exception<rtc_connection_error>(m, "RtcConnectionError");
+    py::register_exception<rtc_list_error>(m, "RtcListError");
 
     // Structs
     py::class_<CardInfo>(m, "CardInfo")
@@ -184,6 +227,16 @@ PYBIND11_MODULE(rtc6_bindings, m)
         .def_property("ip_address", &CardInfo::getIpStr, nullptr)
         .def_property("is_acquired", &CardInfo::getIsAcquired, nullptr);
 
+    py::enum_<ListStatus>(m, "ListStatus")
+        .value("LOAD1", ListStatus::LOAD1)
+        .value("LOAD2", ListStatus::LOAD2)
+        .value("READY1", ListStatus::READY1)
+        .value("READY2", ListStatus::READY2)
+        .value("BUSY1", ListStatus::BUSY1)
+        .value("BUSY2", ListStatus::BUSY2)
+        .value("USED1", ListStatus::USED1)
+        .value("USED2", ListStatus::USED2);
+
     // Real functions which are intended to be used
     m.def("check_connection", &check_conection, "check the active connection to the eth box: throws RtcConnectionError on failure, otherwise does nothing.");
     m.def("connect", &connect, "connect to the eth-box at the given IP", py::arg("ip_string"), py::arg("program_file_path"), py::arg("correction_file_path"));
@@ -191,6 +244,8 @@ PYBIND11_MODULE(rtc6_bindings, m)
     m.def("close_again", &close_connection, "close the open connection, if any");
     m.def("get_card_info", &get_card_info, "get info for the connected card; throws RtcConnectionError on failure");
     m.def("get_last_error", &get_last_eth_error, "get the last error for an ethernet command");
+    m.def("init_list_loading", &init_list_loading, "initialise the given list (1 or 2)", py::arg("list_no"));
+    m.def("get_list_statuses", &get_list_statuses, "get the statuses of the command lists");
 
     // Just for testing - TODO remove these when things are going
     m.def("add", &add, "A function that adds two numbers", py::arg("i"), py::arg("j"));
--- src/rtc6_fastcs/bindings/rtc6_bindings.pyi
+++ src/rtc6_fastcs/bindings/rtc6_bindings.pyi
@@ -4,10 +4,14 @@ bindings for the scanlab rtc6 ethernet laser controller
 
 from __future__ import annotations
 
+import typing
+
 __all__ = [
     "CardInfo",
+    "ListStatus",
     "RtcConnectionError",
     "RtcError",
+    "RtcListError",
     "add",
     "check_connection",
     "close",
@@ -15,6 +19,8 @@ __all__ = [
     "connect",
     "get_card_info",
     "get_last_error",
+    "get_list_statuses",
+    "init_list_loading",
     "ip_int_to_str",
     "ip_str_to_int",
     "throw_rtc_error",
@@ -32,10 +38,62 @@ class CardInfo:
     @property
     def serial_number(self) -> int: ...
 
+class ListStatus:
+    """
+    Members:
+
+      LOAD1
+
+      LOAD2
+
+      READY1
+
+      READY2
+
+      BUSY1
+
+      BUSY2
+
+      USED1
+
+      USED2
+    """
+
+    BUSY1: typing.ClassVar[ListStatus]  # value = <ListStatus.BUSY1: 4>
+    BUSY2: typing.ClassVar[ListStatus]  # value = <ListStatus.BUSY2: 5>
+    LOAD1: typing.ClassVar[ListStatus]  # value = <ListStatus.LOAD1: 0>
+    LOAD2: typing.ClassVar[ListStatus]  # value = <ListStatus.LOAD2: 1>
+    READY1: typing.ClassVar[ListStatus]  # value = <ListStatus.READY1: 2>
+    READY2: typing.ClassVar[ListStatus]  # value = <ListStatus.READY2: 3>
+    USED1: typing.ClassVar[ListStatus]  # value = <ListStatus.USED1: 6>
+    USED2: typing.ClassVar[ListStatus]  # value = <ListStatus.USED2: 7>
+    __members__: typing.ClassVar[
+        dict[str, ListStatus]
+    ]  # value = {'LOAD1': <ListStatus.LOAD1: 0>, 'LOAD2': <ListStatus.LOAD2: 1>, 'READY1': <ListStatus.READY1: 2>, 'READY2': <ListStatus.READY2: 3>, 'BUSY1': <ListStatus.BUSY1: 4>, 'BUSY2': <ListStatus.BUSY2: 5>, 'USED1': <ListStatus.USED1: 6>, 'USED2': <ListStatus.USED2: 7>}
+    @staticmethod
+    def _pybind11_conduit_v1_(*args, **kwargs): ...
+    def __eq__(self, other: typing.Any) -> bool: ...
+    def __getstate__(self) -> int: ...
+    def __hash__(self) -> int: ...
+    def __index__(self) -> int: ...
+    def __init__(self, value: int) -> None: ...
+    def __int__(self) -> int: ...
+    def __ne__(self, other: typing.Any) -> bool: ...
+    def __repr__(self) -> str: ...
+    def __setstate__(self, state: int) -> None: ...
+    def __str__(self) -> str: ...
+    @property
+    def name(self) -> str: ...
+    @property
+    def value(self) -> int: ...
+
 class RtcConnectionError(Exception):
     pass
 
 class RtcError(Exception):
+    pass
+
+class RtcListError(Exception):
     pass
 
 def add(i: int, j: int) -> int:
@@ -71,6 +129,16 @@ def get_card_info() -> CardInfo:
 def get_last_error() -> int:
     """
     get the last error for an ethernet command
+    """
+
+def get_list_statuses() -> list:
+    """
+    get the statuses of the command lists
+    """
+
+def init_list_loading(list_no: int) -> None:
+    """
+    initialise the given list (1 or 2)
     """
 
 def ip_int_to_str(ip_int: int) -> str:
