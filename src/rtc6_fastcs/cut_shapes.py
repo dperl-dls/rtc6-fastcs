edf66df1162678d084e6213ea8e0393877dc762f
co2e14 <christian.orr@diamond.ac.uk>
2024-12-18 14:33:41 +0000

    add custom cylinder

--- src/rtc6_fastcs/cut_shapes.py
+++ src/rtc6_fastcs/cut_shapes.py
@@ -8,6 +8,10 @@ RTC = Rtc6Eth()
 await RTC.connect()
 
 # need to find the conversion of bits to distance. Then can do cylinder(length, width, passes)
-def cylinder_200l_100w(passes):
-    shape = [(-10000,10000,False),(0,5000,True),(20000,5000,True),(20000,-5000,True),(0,-5000,True),(-10000,-10000,True)] * passes
+def cut_cylinder_200l_100w(passes: int):
+    shape = [(-100,100,False),(0,50,True),(200,50,True),(200,-50,True),(0,-50,True),(-100,-100,True)] * passes
     RE(draw_polygon(RTC, shape)
+
+def cut_cylinder(width: int, length: int, passes: int):
+    shape = [(-width,width,False),(0,(width/2),True),(length,(width/2),True),(length,(-width/2),True),(0,(-width/2),True),(-width,-width,True)] * passes
+    RE(draw_polygon(RTC, shape))
